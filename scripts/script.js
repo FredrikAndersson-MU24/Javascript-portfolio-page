dc3dac89645f5aa0a3b3df6858e727a5de3928d6
Fredrik <3tomu24.andefr@folkuniversitetet.nu>
2025-01-15 15:57:25 +0100

    Repo carousel
    Add image element

--- scripts/script.js
+++ scripts/script.js
@@ -9,7 +9,7 @@ const image = document.getElementById("image");
 const repos = [
   {
     id: 1,
-    img: "../img/image.png",
+    img: "../img/exam-typescript-react.png",
     title: "exam-typescript-react",
     description:
       "A simple todo app built using Typescript and React. Built with components and utilizing useEffect React hook to save the array of tasks to local storage when tasks are added, checked or deleted.",
@@ -17,11 +17,27 @@ const repos = [
   },
   {
     id: 2,
-    img: "../img/image.png",
+    img: "../img/java-tictactoe.png",
     title: "Java-TicTacToe",
     description:
-      "A simple Tic-Tac-Toe game written in Java. Textbased and played in the console. Human vs Human or Human vs CPU. Two difficulty levels against the CPU",
+      "A simple Tic-Tac-Toe game written in Java for an individual exam. Textbased and played in the console. Human vs Human or Human vs CPU. Two difficulty levels against the CPU",
     link: "https://github.com/FredrikAndersson-MU24/Java-TicTacToe",
+  },
+  {
+    id: 3,
+    img: "../img/FLIM-phonebook.png",
+    title: "FLIM-Phonebook",
+    description:
+      "A group project built for the final exam in the Java course. Utilizing OOP, CRUD, inheritance and persistant data using FileStream our group of four bulit this textbased phone book application. PArt of the assignment was using UML and Trello to track work.",
+    link: "https://github.com/LindaMU24/FLIM-Phonebook",
+  },
+  {
+    id: 4,
+    img: "../img/mu24-frontend-intro-exam.png",
+    title: "frontend-intro-exam",
+    description:
+      "A simple todo-app. It uses the browser local storage to save the tasks and their status between browser sessions.",
+    link: "https://github.com/FredrikAndersson-MU24/MU24-frontend-intro-exam",
   },
 ];
 
